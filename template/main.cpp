23befe439fa167d782c24d9efde826500bba86c3
GeoDTN <tadewos85@gmail.com>
2018-02-02 09:57:01 -0500

    Power of variadic template. By Tadewos Somano

---
+++ template/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+template<typename T>
+T printStuff(T v){
+    std::cout<<"from base"<<std::endl;
+    return v;
+}
+
+template<typename Tfirst, typename... Trest>
+Tfirst printStuff(Tfirst first, Trest ... rests){
+   std::cout<<"From Main"<<std::endl;
+    return first+printStuff(rests...);
+}
+/*
+template<typename Tfirst>
+Tfirst printThings(){
+    Tfirst t{printStuff(3,4,10,4.5)};
+   return t;
+}
+*/
+int main()
+{
+    std::cout<<printStuff(3,4,10,4.5)<<std::endl;
+
+    return 0;
+}
