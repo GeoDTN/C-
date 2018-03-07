00cf1306d153fde4cae272791d3b9fd2af170a83
GeoDTN <tadewos85@gmail.com>
2018-03-07 12:52:36 +0100

    Constructor and distructor calling order

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
+class A
+{
+public:
+  A()  { cout << "A's constructor called" << endl; }
+  ~A(){ cout << "A's distructor called" << endl;}
+};
+class B
+{
+public:
+  B()  { cout << "B's constructor called" << endl; }
+  ~B(){ cout << "B's distructor called" << endl;}
+};
+class C: public B, public A  // Note the order
+{
+public:
+  C()  { cout << "C's constructor called" << endl; }
+  ~C(){ cout << "C's distructor called" << endl;}
+};
 int main()
 {
-    cout << "Hello World!" << endl;
+    C c;
     return 0;
 }
